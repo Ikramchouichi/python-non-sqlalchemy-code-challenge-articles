92f8c0b7d831795d1cbb36a15d74de918585d3aa
Ikram Chouichi <ikram@netronian.com>
2024-02-29 09:09:59 -0500

    finish lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,155 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        Article.add_new_article(self)
         
+
+    @classmethod
+    def add_new_article(cls, new_instance):
+        cls.all.append(new_instance)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, new_title):
+        if not hasattr(self, '_title'):
+            if isinstance(new_title, str) and 5 <= len(new_title) <= 50:
+                self._title = new_title
+            else:
+                raise ValueError("Title must be between 5 and 50 characters")
+        else:
+            raise AttributeError("Title cannot be changed after it is set")
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, new_author):
+        if isinstance(new_author, Author):
+            self._author = new_author
+        else:
+            raise TypeError("Author must be an instance of Author class")
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if isinstance(new_magazine, Magazine):
+            self._magazine = new_magazine
+        else:
+            raise TypeError("Magazine must be of type Magazine")
+
+    def __repr__(self):
+        return f'<Article title="{self._title}", author="{self._author.name}", magazine="{self._magazine.name}">'     
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+       
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not hasattr(self, "_name"):
+            if isinstance(new_name, str) and len(new_name) > 0:
+                self._name = new_name
+            else:
+                raise ValueError("Name must be longer than 0 characters")
+        else:
+            raise AttributeError("Name cannot be changed after the author is instantiated.")
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self.articles()})
 
     def add_article(self, magazine, title):
-        pass
+        new_article = Article(self, magazine, title)
+        return new_article
 
     def topic_areas(self):
-        pass
+       
+        categories = {article.magazine.category for article in self.articles()}
+        if categories:  
+            return list(categories)
+        else:
+            return None
+    
+    def __repr__(self):
+        return f'<Author name="{self.name}">'
+
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name
+        self._category = category
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and 2 <= len(new_name) <= 16:
+            self._name = new_name
+        else:
+            raise ValueError("Name must be between 2 and 16 characters")
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, new_category):
+        if isinstance(new_category, str) and len(new_category) > 0:
+            self._category = new_category
+        else:
+            raise ValueError("Category must be longer than 0 characters")
 
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
+        return list({article.author for article in self._articles})
 
     def article_titles(self):
-        pass
+
+        titles = [article.title for article in self.articles()]
+        
+        if not titles:
+            return None
+        else:
+            return titles
 
     def contributing_authors(self):
-        pass
+       
+        author_counts = {}
+
+        for article in self.articles():
+    
+            if article.author in author_counts:
+                author_counts[article.author] += 1
+            else:
+                author_counts[article.author] = 1
+
+       
+        contributing_authors = [author for author, count in author_counts.items() if count > 2]
+
+        return None if not contributing_authors else contributing_authors
+
+    def __repr__(self):
+        return f'<Article title="{self._title}", author="{self._author.name}", magazine="{self._magazine.name}">'
--- lib/debug.py
+++ lib/debug.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import ipdb;
 
+
 from classes.many_to_many import Article
 from classes.many_to_many import Author
 from classes.many_to_many import Magazine
@@ -8,6 +9,10 @@ from classes.many_to_many import Magazine
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
-
-    # don't remove this line, it's for debugging!
-    ipdb.set_trace()
+    magazine = Magazine("Vogue", "Fashion")
+    author1 = Author("agatha crusty")
+    author2 = Author("Fyodor Dostoevsky")
+    article1 = Article(magazine, author1, "How to wear a tutu with style")
+    article1 = Article(magazine, author2 , "How to wear a tutu with style")
+    
+ipdb.set_trace()
